12de219c3c6d3e32aa04bdf233c959dd6099126d
kovrus <ruslan@crate.io>
2015-09-21 15:12:09 +0200

    Add image type to launch script

--- github-archive/start-cluster.sh
+++ github-archive/start-cluster.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
 
 if [ "help" == "$1" ] ; then
-  echo "usage: ./startscript <nr-instances> <medium> <key-name>"
+  echo "usage: ./startscript <nr-instances> <key-name>"
   exit 1
 else
     CountInstances=$1
 fi
 
 if [[ -z "$2" ]] ; then
-  echo "Please specify an instance type"
-  exit 1
-fi
-
-if [[ -z "$3" ]] ; then
   echo "Please specify a key-name"
   exit 1
 fi
@@ -20,8 +15,8 @@ fi
 INSTANCE_IDS=$(bin/aws ec2 run-instances \
 --image-id ami-358ba442 \
 --count $CountInstances \
---instance-type $2 \
---key-name $3 \
+--instance-type c3.4xlarge \
+--key-name $2 \
 --user-data $(base64 user-data.sh) \
 --block-device-mappings "[{\"DeviceName\":\"/dev/sdb\",\"VirtualName\":\"ephemeral0\"},{\"DeviceName\":\"/dev/sdc\",\"VirtualName\":\"ephemeral1\"}]" \
 | jq ".Instances[].InstanceId" | tr -d '"')
