6813e95030645607b7616162074a77f3ab74c263
Christian Haudum <christian.haudum@gmail.com>
2015-09-24 12:08:48 +0200

    removed useless FT index from schema
    and made payloads['commits'] and object array

--- github-archive/schema.sql
+++ github-archive/schema.sql
@@ -1,18 +1,25 @@
 CREATE TABLE github (
     type STRING,
-    payload OBJECT AS(
-      commits OBJECT AS (
-        message STRING
-      )
+    payload OBJECT AS (
+      commits ARRAY(OBJECT AS (
+        sha STRING,
+        author OBJECT,
+        message STRING,
+        "distinct" BOOLEAN,
+        url STRING
+      ))
     ),
-    repo OBJECT,
+    repo OBJECT AS (
+      id LONG
+    ),
     actor OBJECT,
     org OBJECT,
     created_at TIMESTAMP,
     month_partition STRING,
     id STRING,
-    public BOOLEAN,
-    INDEX commit_comment_ft using fulltext(payload['commits']['message']) with (analyzer = 'english')
-    ) PARTITIONED BY (month_partition)
-    WITH (number_of_replicas=0,
-            refresh_interval=0);
+    public BOOLEAN
+) PARTITIONED BY (month_partition)
+WITH (
+    number_of_replicas = 0,
+    refresh_interval = 0
+);
